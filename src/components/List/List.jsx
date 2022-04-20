e3f8b356770fcc95f8e620faf0d8c28de80bb3df
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-20 17:32:21 -0400

    scaffolding

---
+++ src/components/List/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListItem from '../ListItem/ListItem'
+
+function List() {
+  return (
+    <ListItem />
+  )
+}
+
+export default List
---
+++ src/components/Navbar/Navbar.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from ".";
+
+test("renders learn react link", () => {
+  render(<Navbar />);
+});
