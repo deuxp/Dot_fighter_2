e166da335e6443e6b36aa6d54cdf6b421f87fc16
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-20 20:33:25 -0400

    render <List/> && initialState

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,35 @@
 import { useState } from "react";
 import "./App.scss";
 import List from "../List/List";
-import ListItem from "../ListItem/ListItem";
+
+const initialState = [
+  {
+    id: 1,
+    body: "hello there, can anybody see me?",
+    created_at: "2022-04-20T19:12:34.877Z",
+  },
+  {
+    id: 2,
+    body: "is any of this matter?",
+    created_at: "2022-04-20T20:30:42.458Z",
+  },
+  {
+    id: 3,
+    body: "Come one come all",
+    created_at: "2022-04-20T20:31:23.973Z",
+  },
+];
 
 function App() {
-  const [state, setState] = useState({});
+  const [state, setState] = useState(initialState);
 
-  return <List />;
+  return (
+    <>
+      <ul>
+        <List messages={state} />
+      </ul>
+    </>
+  );
 }
 
 export default App;
